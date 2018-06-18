676476618570dac33322822051ffb983a37c5657
unknown <bermartinv@gmail.com>
2018-06-18 22:12:15 +0200

    Name, click and image

--- js/cat.js
+++ js/cat.js
@@ -1,19 +1,27 @@
 
 (function(){
     let elements = document.querySelectorAll(".cat");
-    let texts = document.querySelectorAll(".times")
+    let texts = document.getElementById("times");
+    let name = document.getElementById("nameCat");
+    let layout = document.getElementById("screen_img");
+    let address = ["images/cat.jpg","images/cat2.jpg","images/cat3.jpg","images/cat4.jpg","images/cat5.jpg"];
+    let Catname = ["Peter","mini", "minino", "player", "ugly"];
     for (let i = 0; i < elements.length ; i++){
         console.log(elements[i]);
-        elements[i].addEventListener('click',counter(i));
+        elements[i].addEventListener('click',counter(i,address,Catname));
     }
 
-    function counter(i){
-        let count = 0;   
+    function counter(i,address,Catname){
+        let count = 0;
+        let imagen = address[i];
+         
             return function(){
                 count++;
-                texts[i].innerHTML = count;
+                texts.innerHTML = count;
+                layout.setAttribute("src",address[i]);  
+                name.innerHTML = Catname[i];
             }
-        
+        console.log(layout);
         };
 
 })();
